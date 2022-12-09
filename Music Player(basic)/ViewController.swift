99d6946a435d5fe5817ea29db1c2c8cc53b57e6f
Ryan CY <phillin820109@msn.com>
2022-12-09 12:05:45 +0800

    Initial Commit

---
+++ Music Player(basic)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Player(basic)
+//
+//  Created by Ryan Lin on 2022/12/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
